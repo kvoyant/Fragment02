18dee2945bb22c0ffc1d36c572bd25933c4637af
yhkim <kvoyant@gmail.com>
2019-05-18 02:10:39 +0900

    페이지 어댑터에 멤버변수로 프래그먼트를 생성

--- app/src/main/java/com/tjeit/fragment02/MainActivity.java
+++ app/src/main/java/com/tjeit/fragment02/MainActivity.java
@@ -23,6 +23,19 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        act.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                버튼이 눌렀을때 하고 싶은 일?
+
+//                1. 지금 보여지고 있는 Fragment에 접근
+//                => 현재 작성중인 MainActivity 에서?
+
+//                2. 접근한 Fragment에게 어떻게 작업을 시킬건지 ?
+
+            }
+        });
+
 //        뷰페이저 페이지 변경 이벤트 처리
         act.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -32,17 +45,15 @@ public class MainActivity extends BaseActivity {
 
             @Override
             public void onPageSelected(int i) {
-                if(i==0) {
+                if (i == 0) {
                     act.changeFragOneBtn.setText("현재 선택됨");
                     act.changeFragTwoBtn.setText("2번 프래그먼트");
                     act.changeFragThreeBtn.setText("3번 프래그먼트");
-                }
-                else if(i==1) {
+                } else if (i == 1) {
                     act.changeFragOneBtn.setText("1번 프래그먼트");
                     act.changeFragTwoBtn.setText("현재 선택됨");
                     act.changeFragThreeBtn.setText("3번 프래그먼트");
-                }
-                else if(i==2) {
+                } else if (i == 2) {
                     act.changeFragOneBtn.setText("1번 프래그먼트");
                     act.changeFragTwoBtn.setText("2번 프래그먼트");
                     act.changeFragThreeBtn.setText("현재 선택됨");
