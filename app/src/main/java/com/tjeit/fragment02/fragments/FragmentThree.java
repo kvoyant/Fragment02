78c072018f1151c7c5646267898adc2c6e92b700
yhkim <kvoyant@gmail.com>
2019-05-17 18:34:38 +0900

    세번째 프래그먼트 연습문제 + 스크롤뷰 덧대기

---
+++ app/src/main/java/com/tjeit/fragment02/fragments/FragmentThree.java
@@ -0,0 +1,33 @@
+package com.tjeit.fragment02.fragments;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tjeit.fragment02.R;
+import com.tjeit.fragment02.databinding.FragmentThreeBinding;
+
+public class FragmentThree extends Fragment {
+
+    FragmentThreeBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_three, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        binding.threeTextView.setText("세번째 프래그먼트");
+    }
+}
